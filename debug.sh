0e0b668f8fe6d68bd61edcec7b80241bb4ad8403
Abel-Breaker <180770175+Abel-Breaker@users.noreply.github.com>
2026-03-07 21:42:54 +0100

    Added LAStools + Automatic compilation

--- debug.sh
+++ debug.sh
@@ -14,8 +14,31 @@ NC='\033[0m'  # No color / reset
 echo -e "\n${RED}Analyzing code with clang-tidy...${NC}"
 clang-tidy ./src/*.c -checks=*,-llvmlibc-restrict-system-libc-headers -- -Iutils -std=c2x
 
-# Compile with Make
+# Compile
 echo -e "\n\n\n${YELLOW}Compiling...${NC}"
+
+# .a for LAStools
+if [ ! -f src/LAStools/LASlib/lib/libLASlib.a ]; then
+    (cd ./src/LAStools && cmake . && make)
+    rm -rf src/LAStools/CMakeFiles src/LAStools/cmake_install.cmake src/LAStools/CMakeCache.txt src/LAStools/Makefile
+fi
+
+# .a for Reader wrapper
+if [ ! -f src/LAStools/LASlib/lib/libparse_lidar_points.a ]; then
+    g++ -std=c++23 -O0 \
+    -I./src/LAStools/LASlib/inc \
+    -I./src/types \
+    -I./src/LAStools/LASzip/src \
+    -I./src/LAStools/LASzip/include/laszip \
+    -c ./src/utils/parse_lidar_points.cpp \
+    -o ./src/utils/parse_lidar_points.o
+
+    ar rcs ./src/utils/libparse_lidar_points.a ./src/utils/parse_lidar_points.o
+
+    rm ./src/utils/parse_lidar_points.o
+fi
+
+# Compile the final program
 make
 
 # Execute
@@ -24,19 +47,3 @@ echo -e "\n\n\n${GREEN}Executing...${NC}"
 
 # Clean
 make clean
-
-: '
-g++ -std=c++23 \
-  -I./src/LAStools/LASlib/inc \
-  -I./src/types \
-  -I./src/LAStools/LASzip/src \
-  -I./src/LAStools/LASzip/include/laszip \
-  -c ./src/utils/parse_lidar_points.cpp \
-  -o ./src/utils/parse_lidar_points.o
-
-ar rcs ./src/utils/libparse_lidar_points.a ./src/utils/parse_lidar_points.o
-
-
-(cd ./src/LAStools && cmake . && make)
-rm src/LAStools/CMakeFiles src/LAStools/cmake_install.cmake src/LAStools/CMakeCache.txt src/LAStools/Makefile
-'
