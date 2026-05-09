63d30a12ae9c8bbb2e8511ed63ed4613b06a1f3d
Abel-Breaker <180770175+Abel-Breaker@users.noreply.github.com>
2026-05-09 14:11:48 +0200

    refactor

--- src/points_reorder_algorithms/cuthill-mckee.c
+++ src/points_reorder_algorithms/cuthill-mckee.c
@@ -9,7 +9,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-static size_t *degrees_original;
 static size_t *degrees;
 static size_t *indices;
 static size_t counter;
@@ -26,18 +25,6 @@ int compare(const void *a, const void *b)
 	return 0;
 }
 
-int compare_original(const void *a, const void *b)
-{
-	const size_t ia = *(const size_t *)a;
-	const size_t ib = *(const size_t *)b;
-
-	if (degrees_original[ia] < degrees_original[ib])
-		return -1;
-	if (degrees_original[ia] > degrees_original[ib])
-		return 1;
-	return 0;
-}
-
 static inline size_t get_point_index_lowest_degree(const bool *restrict visited, size_t num_points)
 {
 	for (; counter < num_points; ++counter) {
@@ -45,7 +32,7 @@ static inline size_t get_point_index_lowest_degree(const bool *restrict visited,
 			return indices[counter];
 		}
 	}
-	return 0;
+	return SIZE_MAX;
 }
 
 void reorder_cuthill_mckee(const Octree *octree, Points *new_points)
@@ -63,51 +50,43 @@ void reorder_cuthill_mckee(const Octree *octree, Points *new_points)
 	size_t *permutations = malloc(sizeof(*permutations) * num_points);
 	indices = malloc(sizeof(*indices) * num_points);
 	degrees = malloc(sizeof(*degrees) * num_points);
-	degrees_original = malloc(sizeof(*degrees_original) * num_points);
 
 	// Check allocations
-	if (!queue || !visited || !permutations || !degrees || !indices || !degrees_original) {
+	if (!queue || !visited || !permutations || !degrees || !indices) {
 		destroy_queue(queue);
 		free(visited);
 		free(permutations);
 		free(degrees);
 		free(indices);
-		free(degrees_original);
 		handle_error(ERROR_MALLOC, ERR_FATAL, "Cannot allocate auxiliar structures for reorder RCM");
 		return;
 	}
 
-	// Precalculate all points degrees
-	#pragma omp parallel for schedule(static) // TODO: Test with schedule(dynamic)
+// Precalculate all points degrees
+#pragma omp parallel for schedule(static) // TODO: Test with schedule(dynamic)
 	for (size_t i = 0; i < num_points; ++i) {
-		degrees_original[i] = octree_radius_neighbor_count(octree, i, radius); // Mantain a copy of the array without reorder
-		degrees[i] = degrees_original[i];
+		degrees[i] = octree_radius_neighbor_count(octree, i, radius);
 		indices[i] = i;
 	}
 
 	// Preorder all indices depending of the degrees
 	qsort(indices, num_points, sizeof(*indices), compare);
 
-	// Get the first point to visit and prepare for start the algorithm
-	size_t min_grade_point_index = get_point_index_lowest_degree(visited, num_points);
-	visited[min_grade_point_index] = true;
-	enqueue(queue, min_grade_point_index);
-
 	// Start the algorithm RCM to obtain permutations vector
 	size_t points_visited = 0;
-	while (points_visited < num_points) { // Make sure to visit all the points
+	while (true) {
 
 		// If queue is empty, graph is disconnected so pick next unvisited node with lowest degree
 		if (is_queue_empty(queue)) {
-			min_grade_point_index = get_point_index_lowest_degree(visited, num_points);
+			size_t min_degree_point_index = get_point_index_lowest_degree(visited, num_points);
 
 			// Check if all nodes where visited
-			if (min_grade_point_index == 0 && visited[min_grade_point_index] == true) {
+			if (min_degree_point_index == SIZE_MAX) {
 				break;
 			}
-			
-			visited[min_grade_point_index] = true;
-			enqueue(queue, min_grade_point_index);
+
+			visited[min_degree_point_index] = true;
+			enqueue(queue, min_degree_point_index);
 		}
 
 		// Obtain next point
@@ -118,7 +97,7 @@ void reorder_cuthill_mckee(const Octree *octree, Points *new_points)
 		// Get points neighbors
 		RadiusResultOctree result = {};
 		octree_radius_search(octree, index, radius, &result);
-		qsort(result.indices, result.count, sizeof(size_t), compare_original);
+		qsort(result.indices, result.count, sizeof(size_t), compare);
 
 		// Process next point searching for new points to enqueue
 		for (size_t i = 0; i < result.count; ++i) {
@@ -133,9 +112,10 @@ void reorder_cuthill_mckee(const Octree *octree, Points *new_points)
 	// Reserves memory for new points
 	if (!reserve_memory_points(new_points, num_points)) {
 		destroy_queue(queue);
-		free(permutations);
 		free(visited);
+		free(permutations);
 		free(degrees);
+		free(indices);
 		handle_error(ERROR_MALLOC, ERR_FATAL, "Cannot reserve memory for points");
 		return;
 	}
@@ -148,8 +128,8 @@ void reorder_cuthill_mckee(const Octree *octree, Points *new_points)
 
 	// Free memory
 	destroy_queue(queue);
-	free(permutations);
 	free(visited);
+	free(permutations);
 	free(degrees);
 	free(indices);
 }
